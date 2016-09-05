c2f199478a323d3cfdd2de2356b6e252e44be048
Jerome Lachaud <jerome.lachaud@gmail.com>
2016-09-05 17:56:57 +0100

    Remplace 'settings' with the API info by a factory

---
+++ js/lastfmApiFactory.js
@@ -0,0 +1,12 @@
+function lastfmApiFactory() {
+
+  const lastfmApiFactory = {
+    baseUrl: 'http://ws.audioscrobbler.com/2.0/?format=json',
+    user: 'PatBateman75',
+    apiKey: 'ce36645ab9a1ec3fc807f3ef8ef729b7'
+  };
+
+  return lastfmApiFactory;
+}
+
+export { lastfmApiFactory };
